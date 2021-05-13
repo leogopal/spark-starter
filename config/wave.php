80f07ed629ee591c4e4bef074d2f1d645f781533
Leo Gopal <leo@leogopal.com>
2021-05-13 05:18:23 +0200

    env vars

--- config/wave.php
+++ config/wave.php
@@ -16,8 +16,8 @@ return [
 	],
 
 	'user_model' => App\User::class,
-	'show_docs' => env('WAVE_DOCS', true),
-    'demo' => env('WAVE_DEMO', true),
+	'show_docs' => env('WAVE_DOCS', false),
+    'demo' => env('WAVE_DEMO', false),
     'dev_bar' => env('WAVE_BAR', true),
 
     'paddle' => [
